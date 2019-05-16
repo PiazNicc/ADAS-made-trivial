64d1312419aa5c791acf69c40272f48fcd48b943
bellissimo <mhong1654@gmail.com>
2019-05-16 21:31:08 +0200

    Provato a leggere riga per riga salvando in un buffer dato che non so se string esiste

---
+++ readTest.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void readLine(char buffer[],FILE *f,int maxChar){
+
+   while( fgets (buffer,maxChar, f)!=NULL ) {
+      	puts(buffer);
+	//in qualche modo invia il data prima che inizi il prossimo ciclo oppure lo scrive sul file
+	sleep(1); 
+   }
+}
+
+int main(int argc, char *argv[]){
+	FILE *f;
+	int maxChar = 20;
+	char str[maxChar];
+	f = fopen("data.log","r");
+
+	if(f == NULL){
+		printf("File not found");
+		return;
+	}else{
+		readLine(str,f,maxChar);
+	}
+	
+	fclose(f);
+	
+}
