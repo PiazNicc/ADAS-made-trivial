862025e427d2b0d00c9643b55ac60a3a95f539cd
cipo64 <pietrobernaei@gmail.com>
2019-05-16 13:17:58 +0200

    steerbywire without inputmethod

---
+++ steerbywire.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include <unistd.h>
+
+
+ 
+FILE *openFileInput(){
+FILE *steerPunt;
+return steerPunt=fopen("steerIn.log","R");
+}
+FILE *openFileOutput(){
+FILE *steerPunt;
+return steerPunt=fopen("steer.log","w");
+}
+
+void Scrivi(int i,FILE *steerOu)
+{
+
+	char *ma[]={"STO GIRANDO A DESTRA","STO GIRANDO A SINISTRA","NO ACTION"};
+	int maLenght[]={20,22,9};
+
+	
+	int k;
+	if(i!=2)
+	{
+	for(int j=0;j<4;j++)
+	{
+	
+	k=fwrite(ma[i],1,maLenght[i],steerOu);
+	fwrite("\n",1,1,steerOu);
+	sleep(1);
+	}
+	}
+	else
+	{
+	k=fwrite(ma[i],1,maLenght[i],steerOu);
+	fwrite("\n",1,1,steerOu);
+	}
+}
+
+void main()
+{
+	FILE *steerOu=openFileOutput();
+	FILE *steerIn=openFileInput();
+	Scrivi(2,steerOu);
+	Scrivi(0,steerOu);
+	Scrivi(1,steerOu);
+}
+
+
+
