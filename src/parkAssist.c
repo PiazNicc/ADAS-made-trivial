b75b8623052709737f69d8f582a2a672995cf80c
Niccolò Piazzesi <npiazzesi@gmail.com>
2019-05-21 17:21:20 +0200

    tolte librerie non usate

--- src/parkAssist.c
+++ src/parkAssist.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <errno.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/stat.h>
+#include <stdlib.h>
 #include <fcntl.h>
 int main(int argc, char *argv[])
 {
