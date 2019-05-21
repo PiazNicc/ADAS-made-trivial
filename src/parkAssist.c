48d2006284a376f35b9540d116959d0f14456741
Niccolò Piazzesi <npiazzesi@gmail.com>
2019-05-21 17:17:02 +0200

    cosi la scrittura sembra funzionare,da ristrutturare per bene

--- src/parkAssist.c
+++ src/parkAssist.c
@@ -1,32 +1,30 @@
 #include <stdio.h>
 #include <errno.h>
-
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 int main(int argc, char *argv[])
 {
-    FILE *p = fopen("/dev/urandom", "r");
-    FILE *log = fopen("assist.log", "w");
-    //char data[4];
-	int rand=0;
-	 if (p == NULL || log == NULL)
-        {
-            perror("errore in apertura file");
-            exit(-1);
-            /* code */
+	FILE *p = fopen("/dev/urandom", "r");
+	FILE *log = fopen("assist.log", "w");
+	char data[4];
+	int rng = 0;
+	if (p == NULL || log == NULL)
+	{
+		perror("errore in apertura file");
+
+		/* code */
 	}
-	int n=0;
-	while(fread(&rand,1,4,p)==4 && n<10) {
-		//int c= atoi(data);
-		//printf("%d",c);
-		//fwrite(&rand,4,1,log);
-		fprintf(log, "%d",rand);
+	for (int i = 0; i < 4; i++)
+	{
+		rng = getc(p);/* code */
+        fprintf(log,"%d ",rng);
+	}
+	
+	
+	fclose(log);
+	fclose(p);
 
-	//prova con 10 termini
-		n=n+1;
-	//sleep(1); 
-
-   }
-   fclose(log);
-   fclose(p);
-    
-    //exit(0);
+	//exit(0);
 }
