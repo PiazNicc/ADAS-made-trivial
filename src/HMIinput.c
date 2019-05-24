f347e73c26d84866190630ab07fd0a39953fc673
Niccolò Piazzesi <npiazzesi@gmail.com>
2019-05-24 11:07:40 +0200

    script di avvio funzionante,dovrebbe essere ok

---
+++ src/HMIinput.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<errno.h>
+
+int main(int argc,char *argv[]){
+    printf("macchina accesa,scrivere INIZIO per mettere in moto\n\n");
+    printf("quando si vuole parcheggiare,scrivere PARCHEGGIO sul terminale.\n");
+    char input[10];
+    scanf("%s",&input);
+    while(1==1){}
+    exit(0);
+}
