cca38cbfd353de2d5242a9eea466930bf45c1693
Niccolò Piazzesi <npiazzesi@gmail.com>
2019-05-24 11:14:55 +0200

    versione base funzionante input

--- src/HMIinput.c
+++ src/HMIinput.c
@@ -1,12 +1,15 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<errno.h>
+#include<string.h>
 
 int main(int argc,char *argv[]){
     printf("macchina accesa,scrivere INIZIO per mettere in moto\n\n");
     printf("quando si vuole parcheggiare,scrivere PARCHEGGIO sul terminale.\n");
     char input[10];
     scanf("%s",&input);
-    while(1==1){}
+    while(strcmp(input,"PARCHEGGIO") !=0 ){
+         scanf("%s",&input);
+    }
     exit(0);
 }
