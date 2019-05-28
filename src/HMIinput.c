3da73e88009ede732d473c7f0a1da02ebfc30374
Niccolò Piazzesi <npiazzesi@gmail.com>
2019-05-28 10:58:05 +0200

    flusso di esecuzione input modificato,da aggiungere creazioni processi

--- src/HMIinput.c
+++ src/HMIinput.c
@@ -1,15 +1,29 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<errno.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
 
-int main(int argc,char *argv[]){
+int main(int argc, char *argv[])
+{
+    char input[11];
     printf("macchina accesa,scrivere INIZIO per mettere in moto\n\n");
-    printf("quando si vuole parcheggiare,scrivere PARCHEGGIO sul terminale.\n");
-    char input[10];
-    scanf("%s",&input);
-    while(strcmp(input,"PARCHEGGIO") !=0 ){
-         scanf("%s",&input);
+    scanf("%s", &input);
+    while (strcmp(input, "INIZIO") != 0)
+    {
+        printf("\ninput non riconosciuto,scrivere INIZIO\n\n");
+        scanf("%s", &input);
     }
+    //init_system();
+    printf("\nMacchina in moto,digitare PARCHEGGIO quando si vuole avviare la procedura apposita\n\n");
+    scanf("%s", &input);
+    while (strcmp(input, "PARCHEGGIO") != 0)
+    {
+        printf("\ninput non riconosciuto,digitare PARCHEGGIO\n\n");
+        scanf("%s", &input);
+    }
+        
+    
+    //parking()
+    printf("sto parcheggiando\n");
     exit(0);
 }
