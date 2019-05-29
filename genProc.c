16e33975faf11475fe8a155a405487dee15f050b
bellissimo <mhong1654@gmail.com>
2019-05-29 14:30:39 +0200

    Funziona la uccisione dei processi

--- genProc.c
+++ genProc.c
@@ -1,19 +1,30 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <signal.h>
-/*SEMBRA funzionare, ho messo sleep 100 per evitare che muoiano subito i processi, in modo da poter provare la kill*/
+/*Creazione dei processi e inserimento pid nell'array funziona, ma la kill non funziona correttamente,se faccio kill alcuni processi non stampano il messaggio*/
 
+void killProc(int array[]){
+	for(int i=0;i<7;i++){
+		printf("Processo[%d]: %d---\n",i,array[i]);
+		kill(array[i],SIGKILL);   
+		wait();
+	}
+}
 
 int  main(int argc, char *argv[]){
 	printf("Processo input,il mio pid è : %d\n",getpid());
 	int pidECU_S=fork();
+	
 	if(pidECU_S == 0){  //ECU SERVER CREATO
 		printf("Processo ECU Server,il mio pid è : %d , sono figlio di: %d \n",getpid(),getppid());
+		printf("ID del gruppo è: %d\n",getpgid(getpid()));
+
 		int pidECU_C = fork();
 		int pidArray[7];
 		int i=0;
+
 		if(pidECU_C == 0){	//ECU CLIENT
-		/*CODICE del processo, ho usato sleep per testare la kill*/	
+		/*CODICE del processo*/	
 			printf("Processo ECU Client,il mio pid è : %d , sono figlio di: %d \n",getpid(),getppid());
 			sleep(100);
 			
@@ -22,7 +33,7 @@ int  main(int argc, char *argv[]){
 			i++;
 			int pidThrottle = fork();
 			if(pidThrottle == 0){    // THROTTLE
-				/*CODICE del processo, ho usato sleep per testare la kill*/
+				/*CODICE del processo Throttle*/
 				printf("Processo Acceleratore,il mio pid è : %d , sono figlio di: %d \n",getpid(),getppid());
 				sleep(100);
 
@@ -32,8 +43,9 @@ int  main(int argc, char *argv[]){
 
 				int pidSteer = fork();	//Server crea un altro figlio- STEER
 				if(pidSteer ==0){		//STEER
-					/*CODICE del processo, ho usato sleep per testare la kill*/
-					printf("Processo Sterzo,il mio pid è : %d , sono figlio di: %d \n",getpid(),getppid());sleep(100);
+					/*CODICE del processo Steer*/
+					printf("Processo Sterzo,il mio pid è : %d , sono figlio di: %d \n",getpid(),getppid());
+					sleep(100);
 				}else{		
 					pidArray[i] = pidSteer;
 					i++;
@@ -59,7 +71,7 @@ int  main(int argc, char *argv[]){
 							
 							int pidForward = fork();	//Server crea un altro figlio -Forward
 							if(pidForward == 0){		//FORWARD
-							/*CODICE del processo, ho usato sleep per testare la kill*/
+							/*CODICE del processo*/
 								printf("Processo ForwardCamera,il mio pid è : %d , sono figlio di: %d \n",getpid(),getppid());
 								sleep(100);
 								
@@ -68,23 +80,37 @@ int  main(int argc, char *argv[]){
 								i++;
 								int pidBlindSpot = fork();	//Server crea un altro figlio-BlindSpot
 								if(pidBlindSpot == 0){		//BLINDSPOT
-							/*CODICE del processo, ho usato sleep per testare la kill*/
-									printf("Processo BlindSpot, il mio pid è: %d, sono figlio di: %d \n",getpid(),getppid());
-									sleep(100);					
+							/*CODICE del processo*/
+									printf("Processo BlindSpot, il mio pid è: %d, sono figlio di: %d \n",getpid(),getppid()); 
+									sleep(100);
 								}else{
 									pidArray[i] = pidBlindSpot;
-									/*Non sono sicuro che si possa fare qui, ma sembra funzionare*/
-									for(int j=0;j<7;j++){
-										kill(pidArray[j],SIGKILL);
-									}
-								}wait();
-							}wait();				
-						}wait();
-					}wait();
-				}wait();
-			}wait();	
-		}wait();
+									printf("Now ECU is gonna kill the processes\n");
+							/*CODICE DEL PROCESSO ECU SERVER*/
+									killProc(pidArray); //stampa il pid di tutti i processi
+									sleep(100);
+								}
+
+							}	//chiusura else forward
+			
+
+						} //chiusura else windishield
+
+
+					}	// chiusura else brake
+
+				
+				}		//chiusura else steer
+
+
+			}			//chiusura else throttle
 	
-	}wait();sleep(100);
+
+		}		//chiusura else ecu client
+	
+	}else{
+
+		wait();		//codice del processo input
+	}			//chiusura else ecu server
 
 }
