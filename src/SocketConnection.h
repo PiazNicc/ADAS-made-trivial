a08cb96d4b5fca488bf753545d854f138282d504
nicco <npiazzesi@gmail.com>
2019-08-01 11:40:35 +0200

    rinominate funzioni

--- src/SocketConnection.h
+++ src/SocketConnection.h
@@ -1,4 +1,4 @@
-int wServer (int fd,char *line1);
-int rServer (int fd, char *str);
+int writeToSocket (int fd,char *line1);
+int readLineFromSocket (int fd, char *str);
 int make_socket (char *name,int mode);
 
