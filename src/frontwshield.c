6f1707d9bd2b09b5e33d1fc5f04d36232c490c09
cipo64 <pietrobernaei@gmail.com>
2019-08-14 11:18:19 +0200

    inserito i socket

--- src/frontwshield.c
+++ src/frontwshield.c
@@ -1,11 +1,13 @@
 #include<stdlib.h>
 #include<stdio.h>
 #include <unistd.h>
- 
+#include "SocketConnection.h"
+
+
 FILE *openFileInput(){
 FILE *wscPunt;
 wscPunt;
-return wscPunt=fopen("frontCamera.data","rb");
+return wscPunt=fopen("input/frontCamera.data","r");
 
 }
 
@@ -17,16 +19,18 @@ return wscPunt=fopen("camera.log","w");
 
 
 
-void readData(FILE *fin,FILE *fout){
+void readData(FILE *fin,FILE *fout,int clientFD){
 	char buffer[15];
 	while(fgets(buffer,sizeof(buffer),fin)!=NULL){
 		/*codice per inviare il dato*/
+		writeToSocket(clientFD,buffer);
 		writeData(fout,buffer);
-		//sleep(10);
+		sleep(10);
 	}
 }
 void writeData(FILE *Punt,char *buffer)
 {
+	
 	fwrite(buffer,1,strlen(buffer),Punt);  //strlen in questo caso è sicuro perchè fgets mette SEMPRE il terminatore a fine lettura
 	//puts(buffer);
 	
@@ -41,7 +45,7 @@ void main()
 	int error=0;
 	FILE *wscIN=openFileInput();
 	FILE *wscOUT=openFileOutput();
-
+	int clientFd=connectToServer("ecu");
 	
 	if(wscIN==NULL){
 	printf("error not open fileINPUT");
@@ -52,8 +56,7 @@ void main()
 	error++;
 	}
 	if(error==0){
-	readData(wscIN,wscOUT);
-	
+	readData(wscIN,wscOUT,clientFd);
 	}
 	fclose(wscOUT);
 	fclose(wscIN);
