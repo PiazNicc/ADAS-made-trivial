8f17253e81f499ed267f00435313d20e1faaaf68
NIccolò Piazzesi <npiazzesi@gmail.com>
2019-08-14 11:46:43 +0200

    fa

--- src/parkAssist.c
+++ src/parkAssist.c
@@ -7,28 +7,26 @@
 #include <sys/un.h>
 #include <unistd.h>
 #include "SocketConnection.h"
-void parkAssist()
+void main()
 { //dovrebbe essere ok
+	unsigned char data[4];
 	FILE *p = fopen("/dev/urandom", "r");
 	FILE *log = fopen("assist.log", "w");
-	int data, len = sizeof(data), ecuServer;
+	int len = sizeof(data), ecuServer;
 	unsigned int rng = 0;
-	if (p == NULL  || log == NULL)
+	if (p == NULL || log == NULL)
 	{
 		perror("errore in apertura file");
 		exit(EXIT_FAILURE);
-
-	
 	}
 	for (int secs = 0; secs < 30; secs++)
 	{
-//gestione errori da fare
-		rng = getc(p);//devo leggere 4 byte quindi uso un int che è delle stesse dimensioni 
-		data = rng;
-		fprintf(log, "%02x", rng);
-		ecuServer = connectToServer("park");
+		//
+		fread(data, 1, 4, p);
+		fwrite(data, 1, 4, log);
+		ecuServer = connectToServer("ecu");
 		send(ecuServer, &data, len, 0);
-		fprintf(log, "\n");
+		fprintf("\n",log);
 		sleep(1);
 	}
 	fclose(log);
