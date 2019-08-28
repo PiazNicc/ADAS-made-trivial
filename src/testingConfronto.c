229258c445e287bce63e66f75ceddb8896a70133
NIccolò Piazzesi <npiazzesi@gmail.com>
2019-08-28 15:47:59 +0200

    cosi mi fa

--- src/testingConfronto.c
+++ src/testingConfronto.c
@@ -3,39 +3,24 @@
 #include <string.h>
 int forwardFacing(unsigned char *data)
 {
-    
-    FILE *p = fopen("/dev/random", "r");
-    return fread(data,1, 24, p);
 
-     
-
+	FILE *p = fopen("/dev/random", "r");
+	return fread(data, 1, 24, p);
 }
 
-int check(char *data,int n){
-	unsigned int conf[]={0xA00F,0xB072,0x2FA8,0x8359,0xCE23};
-	unsigned char input[2];
-	size_t num = sizeof(conf)/sizeof(conf[0]);
-	printf("starting now\n");
-	for(int i=0;i<n-1;i++){
-		input[0]=data[i];				//ogni tanto ci finiscono valori strani
-		printf("the data 0 is:%X\n",data[0]);
-		input[1]=data[i+1];
-		printf("the data 1 is:%X\n",data[i+1]);
-		printf("cicle n:%d\n",i);
-		for(int j=0;j<num;j++){
-			printf("confront cicle n:%d,the conf is %X:,the input is:%s\n",j,conf[j],input);
-			
-			if(memcmp(input,&conf[j],2)==0){		
-				printf("success");
-				return 0;
-			}
-		}	
-	}
-}
-
-void main(){
+void main()
+{
 	unsigned char data[24];
-	int n= forwardFacing(data);
-	check(data,n);
-
+	FILE *p = fopen("/dev/random", "r");
+	fread(data, 1, 24, p);
+	unsigned char conf[] = {0xA00F, 0xB072, 0x2FA8, 0x8359, 0xCE23};
+	for (int i = 0; i < sizeof data / sizeof data[0]; i++)
+	{
+		char *f = memchr(conf, data[i], sizeof(conf));
+		if (f != NULL)
+		{
+			printf("ciao\n");
+		}
+	}
+	return 0;
 }
