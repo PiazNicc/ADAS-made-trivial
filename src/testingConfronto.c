d1b789d2436864c8b3601520647e38ee7890e805
bellissimo <mhong1654@gmail.com>
2019-08-28 15:10:13 +0200

    da valori strani a volte a data[0] e 1

--- src/testingConfronto.c
+++ src/testingConfronto.c
@@ -1,25 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-char *forwardFacing()
+int forwardFacing(unsigned char *data)
 {
-    char data[24];
+    
     FILE *p = fopen("/dev/random", "r");
-    fread(data,1, 24, p);
+    return fread(data,1, 24, p);
 
-    return data;
+     
 
 }
 
-int check(char *data){
-	unsigned char conf[11]={0xA00F,0xB072,0x2FA8,0x8359,0xCE23};  //non ho idea se si possa inizializzare cosi,2byte x5 + terminatore = 11
-	char input[2];
-
-	for(int i=0;i<strlen(data)-1;i++){
-		input[0]=data[i];
+int check(char *data,int n){
+	unsigned int conf[]={0xA00F,0xB072,0x2FA8,0x8359,0xCE23};
+	unsigned char input[2];
+	size_t num = sizeof(conf)/sizeof(conf[0]);
+	printf("starting now\n");
+	for(int i=0;i<n-1;i++){
+		input[0]=data[i];				//ogni tanto ci finiscono valori strani
+		printf("the data 0 is:%X\n",data[0]);
 		input[1]=data[i+1];
-		for(int j=0;j<strlen(conf)-2;j+2){
-			if(memcmp(input,conf[j],2)==0){
+		printf("the data 1 is:%X\n",data[i+1]);
+		printf("cicle n:%d\n",i);
+		for(int j=0;j<num;j++){
+			printf("confront cicle n:%d,the conf is %X:,the input is:%s\n",j,conf[j],input);
+			
+			if(memcmp(input,&conf[j],2)==0){		
 				printf("success");
 				return 0;
 			}
@@ -28,7 +34,8 @@ int check(char *data){
 }
 
 void main(){
-
-	check(forwardFacing());
+	unsigned char data[24];
+	int n= forwardFacing(data);
+	check(data,n);
 
 }
